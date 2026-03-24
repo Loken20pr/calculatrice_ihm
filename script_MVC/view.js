7c8a45753ab84057b50347eec95d9c2e90de8255
Loken20pr <lois.boucher200@gmail.com>
2026-03-24 15:21:17 +0100

    Correction view.js

--- script_MVC/view.js
+++ script_MVC/view.js
@@ -1,24 +1,19 @@
 class View {
     // Propriétés privées
     #screen;
-    #buttons;
+    #AllButtons;
 
-    constructor() {
-        this.#screen = document.getElementById('screen');
-        // On récupère tous les boutons de la calculatrice
-        this.#buttons = document.getElementsByTagName('button');
-    }
-
-    // Méthode pour mettre à jour l'écran
-    updateDisplay(valeur) {
-        // Sécurité pour ne pas afficher "undefined"
-        if (valeur !== undefined) {
-            this.#screen.innerText = valeur;
-        }
-    }
-
-    // Méthode qui expose les boutons au Contrôleur
-    getButtons() {
-        return this.#buttons;
-    }
+	constructor(){
+		this.#screen = document.getElementById('screen');
+		this.#AllButtons = document.getElementById('button');
+	}
+	
+	// Fonction d'affichage
+	updateDisplay(nombre) {
+		this.#screen.innerText = nombre;
+	}
+	getAllButtons() {
+		return this.#AllButtons;
+	}
+	
 }

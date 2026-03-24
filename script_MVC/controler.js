2f3796b76c8cf39e9edbe6db96f5285675570015
Loken20pr <lois.boucher200@gmail.com>
2026-03-24 15:15:24 +0100

    Premiere ittération MVC

--- script_MVC/controler.js
+++ script_MVC/controler.js
@@ -1,3 +1,24 @@
-class Controler {
-	
+class View {
+    // Propriétés privées
+    #screen;
+    #buttons;
+
+    constructor() {
+        this.#screen = document.getElementById('screen');
+        // On récupère tous les boutons de la calculatrice
+        this.#buttons = document.getElementsByTagName('button');
+    }
+
+    // Méthode pour mettre à jour l'écran
+    updateDisplay(valeur) {
+        // Sécurité pour ne pas afficher "undefined"
+        if (valeur !== undefined) {
+            this.#screen.innerText = valeur;
+        }
+    }
+
+    // Méthode qui expose les boutons au Contrôleur
+    getButtons() {
+        return this.#buttons;
+    }
 }
